53293d62fa0df16e793561d8cbc787b1bab73e06
SamisNF <sanefre@gmail.com>
2021-08-16 10:39:15 -0500

    Big Push
    First draft of the operational app is complete.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World")
+//Get the string from <input id="userString">
+//controller function
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    //write a loop that runs backwards
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+
+}
+
+
+//Display the reversed string
+//view function
+function displayString(revString){
+
+    document.getElementById("msg").innerHTML = `Your string reversed is ${revString}.`;
+
+    document.getElementById("alert").classList.remove("invisible");
 }
